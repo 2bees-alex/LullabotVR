a17d80aae642ae160bb1daa1313b2a1da00efb8e
Jerad Bitner <sirkitree@gmail.com>
2014-10-31 13:04:17 -0600

    adding arch and boat

--- assets/js/click.js
+++ assets/js/click.js
@@ -1,16 +1,57 @@
+// list of memes in our scene
+var memes = [
+  'awesome',
+  'bens-castle-2',
+  'bens-castle',
+  'brock-claps',
+  'brock-throwmoney',
+  'chef-yumyum',
+  'deja-micah',
+  'eatoncon',
+  'eric-duran',
+  'erics-jacket',
+  'fucking-rustic',
+  'geek-fashion',
+  'joes-hugs',
+  'juampy-didntread',
+  'kyle-day',
+  'sally-baby',
+  'strap-rocket'
+],
+  mlength = memes.length,
+  cmlength = 0,
+  clicked_memes = [],
+  global_time_passed = 0;
 
-room.buttonClick = function() {
-  log('test');
+room.buttonClick = function (id) {
+  room.objects['console1'].text = typeof id;
+  
+  // see if this was clicked already
+  if (typeof clicked_memes[id] == 'undefined') {
+    // record the id of what was clicked
+    // .push() returns the new length of the array
+    cmlength = clicked_memes.push(id);
+    clicked_memes[id]++;
+    // room.objects[id].pos = translate(player.pos, Vector(-clicked_memes[id]))
+  }
+
+  // room.objects['console'].text = cmlength + " " + clicked_memes;
+
+  // compare the clicked_memes with the list of memes
+  if (memes.length = clicked_memes.length) {
+    room.objects['console'].text = "Congrats! You found all " + memes.length + "memes!";
+    // todo: reposition all images & makes them swirl around the 'player'
+  }
+
 }
 
-function log (value) {
-  room.createObject("Text", {
-    js_id : 'console'
-  });
-  room.objects['console'].pos = Vector(-1, 0, -.1);
-  room.objects['console'].fwd = Vector(-1, 0, 0);
-  room.objects['console'].scale = Vector(5, 5, 5);
-  room.objects['console'].col = Vector(.5, 1, .5);
-  //xdir="-0.725 -0.004 -0.688" ydir="-0.688 0.004 0.725" zdir="0.000 1.000 -0.006"
-  room.objects['console'].text = "This is new text with js_id: " + room.objects['console'].js_id;
+room.update = function (dt) {
+  // setting an elements .pos to this will keep in in the user's view
+  var console_pos = translate(player.pos, Vector(1));
+
+  // set the console to follow the player
+  room.objects['console'].pos = translate(player.pos, Vector(1));
+  room.objects['console'].text = "You have found " + cmlength + " of " + mlength + " memes.";
+
+  // room.objects['console'].fwd = player.view_dir;
 }
