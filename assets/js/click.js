49e609a14ba20b74f88f09075f4274583a7cf66e
sirkitree <sirkitree@gmail.com>
2014-11-03 21:00:45 -0700

    Adding sound

--- assets/js/click.js
+++ assets/js/click.js
@@ -25,7 +25,7 @@ var memes = [
 
 room.buttonClick = function (id) {
   room.objects['console1'].text = typeof id;
-  
+
   // see if this was clicked already
   if (typeof clicked_memes[id] == 'undefined') {
     // record the id of what was clicked
@@ -40,6 +40,7 @@ room.buttonClick = function (id) {
   // compare the clicked_memes with the list of memes
   if (memes.length === clicked_memes.length) {
     room.objects['console'].text = "Congrats! You found all " + memes.length + "memes!";
+
     // todo: reposition all images & makes them swirl around the 'player'
   }
 
@@ -50,11 +51,11 @@ room.update = function (dt) {
   var console_pos = translate(player.pos, Vector(1));
 
   // Rudimentary HUD, follows the player and faces the player.
-  room.objects['console'].text = "You have found " + cmlength + " of " + mlength + " memes.";
+  room.objects['console'].text = "You have found " + cmlength + " of " + mlength + " memes. room:" + window.location;
 
    // I need to create a function that handles this.
   var offsetPosition = translate(player.pos, player.view_dir);
-  var faceUserFWD = scalarMultiply(player.view_dir, -1); 
+  var faceUserFWD = scalarMultiply(player.view_dir, -1);
 
   room.objects["console"].pos = translate(offsetPosition, Vector(0, 1.40, 0));
   room.objects["console"].fwd = faceUserFWD;
@@ -74,7 +75,7 @@ function cleanUpVectorValues (vector, precision) {
 }
 
 // Changing values makes for a weird effect where the text will scale up as there are fewer numbers,
-// and scale down as there are more. after some research, its floating point numbers in JS, 
+// and scale down as there are more. after some research, its floating point numbers in JS,
 // just the nature of the beast.
 function cleanUpFloat(value, precision) {
   return Math.round(value * precision) / precision;
